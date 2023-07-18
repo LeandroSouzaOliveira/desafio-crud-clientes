9eb46f0de1105553feebc6e3bdd263fe1e9f75aa
LeandroSouzaOliveira <leandro.oliveirasouza1@gmail.com>
2023-07-18 11:35:51 -0300

    Finished the verbs GET ALL, POST, PUT and DELETE

--- src/main/java/com/devsuperior/crudclientes/services/ClientService.java
+++ src/main/java/com/devsuperior/crudclientes/services/ClientService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -29,6 +30,35 @@ public class ClientService {
         Client client = result.get();
         ClientDTO dto = new ClientDTO(client);
         return dto;
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        copyDtoEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        Client entity = repository.getReferenceById(id);
+        copyDtoEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    private void copyDtoEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
     }
 
 }
